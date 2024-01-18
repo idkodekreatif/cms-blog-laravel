59aa11c50383e4f5d81d3545c801cf90aee9c7ab
idkodekreatif <id.kodekreatif@gmail.com>
2024-01-18 22:12:43 +0700

    feat(card): fiks card interfaces list data

--- resources/views/back/categiries/index.blade.php
+++ resources/views/back/categiries/index.blade.php
@@ -1,100 +1,101 @@
 <x-apps-layouts title="Categories">
     @push('styles')
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/css/bootstrap.min.css"
+        rel="stylesheet" />
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.7/css/dataTables.bootstrap5.min.css">
     @endpush
-    <div class="row">
-        <div class="col-12">
-            <div class="card mb-4">
-                <div class="card-header pb-0">
-                    <div class="row">
-                        <div class="col-6 d-flex align-items-center">
-                            <h6>Categories table</h6>
-                        </div>
-                        <div class="col-6 text-end">
-                            <!-- Button trigger modal -->
-                            <button type="button" class="btn btn-outline-primary" data-bs-toggle="modal"
-                                data-bs-target="#categoriesCreate">
-                                Add new
-                            </button>
-                        </div>
-                    </div>
+    <div class="card p-4 shadow">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-6 d-flex align-items-center">
+                    <h6>Categories Table</h6>
                 </div>
-                <div class="card-body px-0 pt-0 pb-2">
-                    <div class="table-responsive p-0">
-                        <table class="table align-items-center mb-0">
-                            <thead>
-                                <tr>
-                                    <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
-                                        No
-                                    </th>
-                                    <th
-                                        class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">
-                                        Name</th>
-                                    <th
-                                        class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
-                                        Slug</th>
-                                    <th
-                                        class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
-                                        Created at</th>
-                                    <th
-                                        class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
-                                        Action</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                @foreach ($categories as $category)
-                                <tr>
-                                    <td>
-                                        <div class="d-flex px-2 py-1">
-                                            <div class="d-flex flex-column justify-content-center">
-                                                <p class="text-xs text-secondary font-weight-bold mb-0">{{
-                                                    $loop->iteration }}</p>
-                                            </div>
-                                        </div>
-                                    </td>
-                                    <td>
-                                        <p class="text-xs font-weight-bold mb-0">{{ $category->name }}</p>
-                                    </td>
-                                    <td class="align-middle text-center text-sm">
-                                        <p class="text-xs text-secondary mb-0">{{ $category->slug }}</p>
-                                    </td>
-                                    <td class="align-middle text-center">
-                                        <span class="text-secondary text-xs font-weight-bold">{{ $category->created_at
-                                            }}</span>
-                                    </td>
-                                    <td class="align-middle text-center">
-                                        <!-- Button trigger modal -->
-                                        <button type="button" class="font-weight-bold text-xs text-warning btn btn-sm"
-                                            data-bs-toggle="modal"
-                                            data-bs-target="#categoriesUpdate{{ $category->id }}"><i
-                                                class="fas fa-pencil-alt me-1"></i>
-                                            Update
-                                        </button>
+                <div class="col-6 text-end">
+                    <button type="button" class="btn btn-outline-primary" data-bs-toggle="modal"
+                        data-bs-target="#categoriesCreate">
+                        Add New
+                    </button>
+                </div>
+            </div>
 
-                                        <form action="{{ route('categories.destroy', $category->id) }}" method="post"
-                                            style="display:inline">
-                                            @csrf
-                                            @method('DELETE')
-                                            <button type="submit"
-                                                class="font-weight-bold text-xs text-danger btn btn-sm"
-                                                onclick="return confirm('Are you sure you want to delete this category {{ $category->name }}?')"><i
-                                                    class="far fa-trash-alt me-1"></i>Delete</button>
-                                        </form>
-                                    </td>
-                                </tr>
-                                @endforeach
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
+            <div class="table-responsive p-0 mt-3">
+                <table class="table align-items-center mb-0" id="datatable">
+                    <thead>
+                        <tr>
+                            <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">No</th>
+                            <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">Name
+                            </th>
+                            <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                                Slug
+                            </th>
+                            <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                                Created
+                                at</th>
+                            <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                                Action
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($categories as $category)
+                        <tr>
+                            <td>
+                                <div class="d-flex px-2 py-1">
+                                    <div class="d-flex flex-column justify-content-center">
+                                        <p class="text-xs text-secondary font-weight-bold mb-0">{{ $loop->iteration }}
+                                        </p>
+                                    </div>
+                                </div>
+                            </td>
+                            <td>
+                                <p class="text-xs font-weight-bold mb-0">{{ $category->name }}</p>
+                            </td>
+                            <td class="align-middle text-center text-sm">
+                                <p class="text-xs text-secondary mb-0">{{ $category->slug }}</p>
+                            </td>
+                            <td class="align-middle text-center">
+                                <span class="text-secondary text-xs font-weight-bold">{{ $category->created_at }}</span>
+                            </td>
+                            <td class="align-middle text-center">
+                                <button type="button" class="font-weight-bold text-xs text-warning btn btn-sm"
+                                    data-bs-toggle="modal" data-bs-target="#categoriesUpdate{{ $category->id }}">
+                                    <i class="fas fa-pencil-alt me-1"></i> Update
+                                </button>
+                                <form action="{{ route('categories.destroy', $category->id) }}" method="post"
+                                    style="display:inline">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit" class="font-weight-bold text-xs text-danger btn btn-sm"
+                                        onclick="return confirm('Are you sure you want to delete this category {{ $category->name }}?')">
+                                        <i class="far fa-trash-alt me-1"></i> Delete
+                                    </button>
+                                </form>
+                            </td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
             </div>
         </div>
     </div>
-
-    <!-- Include the modal component -->
+    <!-- Include the modal components -->
     <x-categories-create-modal />
-
     <x-categories-update-modal :categories="$categories" />
 
     @push('scripts')
+    <script src="https://code.jquery.com/jquery-3.7.0.js"></script>
+    <script src="https://cdn.datatables.net/1.13.7/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.13.7/js/dataTables.bootstrap5.min.js"></script>
+    <script>
+        $(document).ready(function() {
+                $('#datatable').DataTable({
+                    "order": [], // You can specify default sorting here if needed
+                    "columnDefs": [{
+                        "targets": 'no-sort', // Add 'no-sort' class to the th where you don't want sorting
+                        "orderable": false,
+                    }]
+                });
+            });
+    </script>
     @endpush
 </x-apps-layouts>
